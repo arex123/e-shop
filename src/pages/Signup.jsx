826b48b1d709e161c27ef3a582f0ab9e4fdd1378
arex123 <ad47kumar@gmail.com>
2024-10-23 23:15:13 +0530

    added

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,5 +1,5 @@
-import Header from "../components/Layouts/Header";
-import Footer from "../components/Layouts/Footer";
+import Header from "../components/Header/Header.jsx";
+import Footer from "../components/Footer.jsx/Footer.jsx";
 import Signup from "../components/Auth/Signup";
 
 const SignupPage = () => {
